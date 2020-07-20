a0a347dbc23ac3e21c9c4aa61eafbc431ed13661
Tunmise Ogunniyi <ogunniyitunmise@gmail.com>
2020-07-20 19:48:52 +0100

    Add SEO properties to 404 page

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -1,5 +1,6 @@
 import React from "react"
 import Layout from "../components/Layout"
+import SEO from "../components/seo"
 
 interface Props {
   uri: string
@@ -7,6 +8,7 @@ interface Props {
 
 const PageNotFound = ({ uri }: Props) => (
   <Layout uri={uri}>
+    <SEO title="404" />
     <div className="content-wrap index-page">
       <h2>Page Not Found</h2>
     </div>
